37ee70fc54aab598e1d79c03165a3ac4c5390504
Alasdair Rothnie <alasdair.rothnie@gmail.com>
2019-04-16 19:15:18 -0700

    third commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
-var app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello Express!</h1>');
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello Express!</h1>');
+// });
+
+app.listen(port, () => {
+    console.log(`Server is up on the port ${port}`);
 });
-
-app.listen(8080);
 
